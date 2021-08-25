c8cf1692a0b493e498301facc45e898de2f9f4a3
mart-j <m.jukss@gmail.com>
2021-08-25 20:34:47 +0300

    added mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8000;
-app.get('/', (_, res) => res.send('Express + TypeScript Server'));
+app.get('/', (_, res) => res.send('Express + MongoDB + TypeScript Server'));
 
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+
+
+(async () => {
+    const app = express();
+
+    await mongoose.connect('mongodb://localhost:27017/node-server');
+
+    console.log("🚀[database] Connected to mongodb://localhost:27017/node-server")
+
+    app.listen(PORT, () => {
+        console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    });
+
+})();
